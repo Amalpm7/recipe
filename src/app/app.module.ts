939cd113cb5cffea91fadd3bdb88ee2a6f6985c5
Amalpm7 <amal.p@nestgroup.net>
2022-10-19 19:21:33 +0530

    add-view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AddReceipeComponent } from './add-receipe/add-receipe.component';
+import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from '@angular/router';
+import { ViewReceipeComponent } from './view-receipe/view-receipe.component';
 
+const appRoutes:Routes=[
+  {
+    path:"",component:AddReceipeComponent
+  },
+  {
+    path:"view",component:ViewReceipeComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AddReceipeComponent,
+    ViewReceipeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
